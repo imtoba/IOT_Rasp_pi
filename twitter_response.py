e1dc5594543268129086e2c4061b725bbb2a4264
T.O.B.A <28225784+imtoba@users.noreply.github.com>
2018-05-21 21:22:33 +0530

    Terminal Screen Python Code
    Part Copy of What happened on my Terminal Screen when Raspberry Pi tweeted by itself as per set criteria!

--- twitter_response.py
+++ twitter_response.py
@@ -21,7 +21,7 @@ from twython import TwythonStreamer
 ... 
 
 >>> stream=MyStreamer(c_k,c_s,a_t,a_s)
->>> stream.statuses.filter(track="Ian G. Harris/Any Search String On Your Twitter Page")
+>>> stream.statuses.filter(track="Ian G. Harris/Any Search String for Your Twitter Page")
 
 Ian G. Harris is popular! #OUTPUT For Third time
 Ian G. Harris is popular! #OUTPUT for Fourth time
